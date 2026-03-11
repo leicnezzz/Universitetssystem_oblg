43fc16bdb5d7761f4c4fd6523d80476e588bf608
leicnez <emmaml@uia.no>
2026-03-11 17:08:29 +0100

    fiksa at menyen kom opp

--- Universitetssystem_oblg/obj/Debug/net10.0/Universitetssystem_oblg.AssemblyInfo.cs
+++ Universitetssystem_oblg/obj/Debug/net10.0/Universitetssystem_oblg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Universitetssystem_oblg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f89c284dcfbc1526494de8ab36782596adac97a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Universitetssystem_oblg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Universitetssystem_oblg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
